7d3762d0b599244a0cb1b1bea972d9fc4f35a291
Venkatesh kumar <167079967+Venky6520@users.noreply.github.com>
2025-02-27 18:06:22 +0530

    initial commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Blog() {        // This is the Blog component
+    return <h1>Blog</h1>                        // This is the JSX that will be rendered
+}                                               // This is the end of the Blog component
---
+++ src/app/products/[productid]/layout.tsx
@@ -0,0 +1,10 @@
+export default function ProductLayout({
+    children,
+}:{children: React.ReactNode}) {
+    return(
+        <>
+        {children}
+        <h2>products features</h2>
+        </>
+    )
+}
